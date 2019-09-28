978969213fb3d4b7589a67c6857192c14305a976
Deepak-rathor <49443697+Deepak-rathor@users.noreply.github.com>
2019-09-28 23:53:15 +0530

    using break function

---
+++ break.py
@@ -0,0 +1,19 @@
+x=int(input("How many candy do you want?\n"))
+total=7
+i=1
+while i<=x:
+    if i>total:
+        print("only {} candy available".format(total))
+        break
+    print("candy")
+    i=i+1
+print("-----  Have a nice day  -----")
+x=int(input("How many candy do you want?\n"))
+total=7
+i=1
+while i<=x:
+    if i>total:
+        print("only {} candy available".format(total))
+        break
+    print("candy")
+    i=i+1
